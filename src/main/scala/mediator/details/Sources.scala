1bd5e85cfc64a5f218651f40359d0aa08c94c2b3
kapinuss <stanislav.kapinus@gmail.com>
2018-12-10 20:05:15 +0300

    mediator-monday

--- src/main/scala/mediator/details/Sources.scala
+++ src/main/scala/mediator/details/Sources.scala
@@ -3,7 +3,7 @@ package mediator.details
 import java.nio.file.Paths
 import akka.http.scaladsl.Http
 import akka.stream.IOResult
-import akka.stream.scaladsl.{FileIO, Sink, Source}
+import akka.stream.scaladsl.{FileIO, Source}
 import akka.util.ByteString
 import scala.concurrent.Future
 
@@ -12,8 +12,6 @@ object Sources {
   import mediator.Mediator.system
 
   def fileSource(file: String): Source[ByteString, Future[IOResult]] = FileIO.fromPath(Paths.get(file))
-
-  def fileSink(file: String): Sink[ByteString, Future[IOResult]] = FileIO.toPath(Paths.get(file))
 
   def httpSource(port: Int): Source[Http.IncomingConnection, Future[Http.ServerBinding]] =
     Http().bind(interface = "localhost", port = port)
