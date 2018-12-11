893f9ac9c49e56cb0c8e65f9f02f9b24877edb29
kapinuss <stanislav.kapinus@gmail.com>
2018-12-11 14:05:18 +0300

    mediator-tuesday

--- src/main/scala/mediator/Mediator.scala
+++ src/main/scala/mediator/Mediator.scala
@@ -3,7 +3,7 @@ package mediator
 import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, ClosedShape}
-import akka.stream.scaladsl.{Broadcast, GraphDSL, RunnableGraph, Sink}
+import akka.stream.scaladsl.{Broadcast, GraphDSL, Merge, RunnableGraph}
 import akka.util.ByteString
 import mediator.details.{Sinks, Sources}
 import scala.concurrent.ExecutionContextExecutor
@@ -14,22 +14,25 @@ object Mediator extends App {
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
-  val bindingFuture = Sources.httpSource(8080).to(Sink.foreach { connection =>
-      println("Accepted new connection from " + connection.remoteAddress)
-      connection.handleWithSyncHandler(http.HttpRoute.requestHandler)
-    })
-
-
   val graph = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
     import GraphDSL.Implicits._
 
     val broadcast = builder.add(Broadcast[ByteString](2))
-    val source = Sources.byteString("test")
+    val merge = builder.add(Merge[ByteString](5))
+    val sourceSingleString = Sources.byteString("")
+    val sourceFile = Sources.fileSource("")
+    val sourceFile2 = Sources.fileSource("")
+    val sourceSingleString2 = Sources.byteString("")
+    val sourceSingleString3 = Sources.byteString("")
 
-    source ~> broadcast ~> Sinks.fileSink("test1.txt")
-    broadcast ~> Sinks.fileSink("test2.txt")
+    sourceFile ~> merge ~> broadcast ~> Sinks.fileSink("")
+    sourceSingleString ~> merge
+    sourceSingleString2 ~> merge
+    sourceSingleString3 ~> merge
+    sourceFile2 ~> merge
+    broadcast ~> Sinks.fileSink("")
 
     ClosedShape
-  })
+  }).run
 
 }
--- src/main/scala/mediator/http/HttpRoute.scala
+++ src/main/scala/mediator/http/HttpRoute.scala
@@ -1,7 +1,12 @@
 package mediator.http
 
+import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.HttpMethods.GET
 import akka.http.scaladsl.model._
+import akka.stream.scaladsl.{RunnableGraph, Sink}
+import mediator.details.Sources
+import mediator.http
+import scala.concurrent.Future
 
 object HttpRoute {
 
@@ -11,11 +16,15 @@ object HttpRoute {
     case request@HttpRequest(GET, Uri.Path("/"), _, _, _) =>
       HttpResponse(entity = HttpEntity(ContentTypes.`text/html(UTF-8)`,
         s"<html><body>Hello world, ${request._2}!</body></html>"))
-
     case HttpRequest(GET, Uri.Path("/crash"), _, _, _) => sys.error("BOOM!")
-
     case r: HttpRequest =>
       r.discardEntityBytes()
       HttpResponse(404, entity = "Unknown resource!")
   }
+
+  val server: RunnableGraph[Future[Http.ServerBinding]] =
+    Sources.httpSource(port = 8080).to(Sink.foreach { connection =>
+      println("Accepted new connection from " + connection.remoteAddress)
+      connection.handleWithSyncHandler(http.HttpRoute.requestHandler)
+    })
 }
