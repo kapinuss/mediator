96698fc9d6d28d06709acf5664420ef38e22b090
Stanislav Kapinus <stanislav.kapinus@gmail.com>
2018-12-14 17:33:37 +0300

    mediator-thursday (#7)
    * mediator-thursday
    
    * mediator-thursday

--- src/main/scala/mediator/details/Sinks.scala
+++ src/main/scala/mediator/details/Sinks.scala
@@ -1,7 +1,6 @@
 package mediator.details
 
 import java.nio.file.Paths
-import akka.NotUsed
 import akka.stream.IOResult
 import akka.stream.scaladsl.{FileIO, Flow, Sink}
 import akka.util.ByteString
@@ -10,7 +9,5 @@ import scala.concurrent.Future
 object Sinks {
 
   def fileSink(file: String): Sink[ByteString, Future[IOResult]] = FileIO.toPath(Paths.get(file))
-
-  def requestSink: Flow[String, Unit, NotUsed] = Flow.fromFunction[String, Unit](() -> mediator.Mediator.selfRequest())
 
 }
